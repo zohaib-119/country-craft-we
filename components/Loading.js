218c4626cd2d8042e55d8ce9267d7938319563d7
Muhammad-Zohaib6 <zohaibmian119@gmail.com>
2024-12-30 00:38:23 +0500

    loader-text-rm

--- components/Loading.js
+++ components/Loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="flex flex-col items-center">
         <FaSpinner className="animate-spin text-orange-500 text-5xl" />
-        <p className="text-orange-500 text-lg mt-4">Loading, please wait...</p>
+        {/* <p className="text-orange-500 text-lg mt-4">Loading, please wait...</p> */}
       </div>
     </div>
   );
